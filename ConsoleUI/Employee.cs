f14de47b38b49e258faa4309c964dbfeb379693a
Karthick Raja S <2104273@cognizant.com>
2024-08-13 13:02:02 +0530

    Add project files.

---
+++ ConsoleUI/Employee.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class Employee
+    {
+        public string EmpId { get; set; }
+        public string? Mode { get; set;}
+        public string? In0rOut { get; set;}
+        public bool Antipass { get; set;}
+        public bool ProxyWork { get; set;}
+        public DateTime DateTime { get; set;}
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleUI;
+using System.Globalization;
+
+string FilePath = "C:\\CTS Projects\\ConsoleUI\\RawData.txt";
+List<string> Lines = File.ReadAllLines(FilePath).ToList();
+List<Employee> Employees = new List<Employee>();
+
+foreach (string line in  Lines)
+{
+    if (line == Lines[0]) continue;
+    else
+    {
+        string[] input = line.Split('\t');
+        Employee _employee = new Employee();
+        _employee.EmpId = input[0];
+        _employee.Mode = input[1];
+        _employee.In0rOut = input[2];
+        _employee.Antipass = (input[3]=="1");
+        _employee.ProxyWork = (input[4]=="1");
+        //_employee.DateTime = Convert.ToDateTime(input[5]);
+        _employee.DateTime = DateTime.ParseExact(input[5], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+        Employees.Add(_employee);
+    }
+}
+
+foreach (Employee employee in Employees)
+{
+    Console.WriteLine($"EnNo:{employee.EmpId}   " +
+        $"Mode:{employee.Mode}    " +
+        $"In/Out:{employee.In0rOut}   " +
+        $"Antipass:{employee.Antipass} " +
+        $"ProxyWork:{employee.ProxyWork} " +
+        $"DateTime:{employee.DateTime.ToString("yyyy-MM-dd HH:mm:ss")}");
+}
+Console.ReadKey();
